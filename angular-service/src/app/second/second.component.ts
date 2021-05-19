d08505c7d703ba5a404ab7ec41c81e96bebd240b
Hilda Amponsah <hilda.amp@hotmail.com>
2021-05-19 12:45:04 +0100

    second- doing the same thing but added id value

--- angular-service/src/app/second/second.component.ts
+++ angular-service/src/app/second/second.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MyCustomService } from '../custom.service'
 @Component({
   selector: 'app-second',
   templateUrl: './second.component.html',
   styleUrls: ['./second.component.css']
 })
 export class SecondComponent implements OnInit {
-
+  msg:string="";
   constructor() { }
 
   ngOnInit(): void {
+  }
+  passName(name:any){
+    let ser = new MyCustomService();
+    this.msg = ser.sayHello(name.value)
   }
 
 }
