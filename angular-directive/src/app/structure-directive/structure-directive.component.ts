cc67540cca12d327d261c41902aeac2a62d61331
Hilda Amponsah <hilda.amp@hotmail.com>
2021-05-19 09:11:53 +0100

    angular directives -changes made in structure-directive html and ts

--- angular-directive/src/app/structure-directive/structure-directive.component.ts
+++ angular-directive/src/app/structure-directive/structure-directive.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-structure-directive',
   templateUrl: './structure-directive.component.html',
   styleUrls: ['./structure-directive.component.css']
 })
 export class StructureDirectiveComponent implements OnInit {
-
+  flag:boolean=false;
+  b1:string="show";
+  
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  change(){
+    if(this.b1=="show"){
+      this.b1 ="hide"
+      this.flag = true;
+    }else{
+      this.flag=false;
+      this.b1="show"
+    }
+  }
 }
