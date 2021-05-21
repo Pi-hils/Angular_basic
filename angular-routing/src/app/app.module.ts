6281cdfd4847d617660c52a698e70d2aedfad0e4
Hilda Amponsah <hilda.amp@hotmail.com>
2021-05-21 10:45:23 +0100

    adding a checkLoginDetails function on .ts and .html

--- angular-routing/src/app/app.module.ts
+++ angular-routing/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { FeedbackComponent } from './feedback/feedback.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -22,7 +24,9 @@ import { RegisterComponent } from './register/register.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-routing/src/app/login/login.component.ts
+++ angular-routing/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{FormGroup, FormControl } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginRef = new FormGroup({
+    user:new FormControl(),
+    pass:new FormControl()
+  });
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  checkLoginDetails() {
+    let login = this.loginRef.value;
+    console.log(login);        
   }
 
 }
