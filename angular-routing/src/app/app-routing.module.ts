7b6bcffd4c1d27cd7397542d7ddd79c43738f545
Hilda Amponsah <hilda.amp@hotmail.com>
2021-05-21 09:48:51 +0100

    adding all component route to app-routing.model and importing

--- angular-routing/src/app/app-routing.module.ts
+++ angular-routing/src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { ContactusComponent } from './contactus/contactus.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path: "aboutus", component:AboutusComponent},
-  {path: "contactus", component:ContactusComponent}
+  {path: "contactus", component:ContactusComponent},
+  {path: "feedback", component:FeedbackComponent},
+  {path: "login", component:LoginComponent},
+  {path: "register", component:RegisterComponent}
 ];
 
 @NgModule({
