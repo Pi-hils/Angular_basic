07af728da7dd95d1297fa7df50ddf1897016ecf3
Hilda Amponsah <hilda.amp@hotmail.com>
2021-05-21 11:22:10 +0100

    adding dashboard route to app-routing and import

--- angular-routing/src/app/app-routing.module.ts
+++ angular-routing/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { ContactusComponent } from './contactus/contactus.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -11,7 +12,8 @@ const routes: Routes = [
   {path: "contactus", component:ContactusComponent},
   {path: "feedback", component:FeedbackComponent},
   {path: "login", component:LoginComponent},
-  {path: "register", component:RegisterComponent}
+  {path: "register", component:RegisterComponent},
+  {path: "home", component:DashboardComponent},
 ];
 
 @NgModule({
